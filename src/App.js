4a94dca677f912f5c4848fe8696da1705351b8a4
Cihat <copurcihat@gmail.com>
2022-10-12 18:22:48 +0300

    structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 
 import './App.css';
+import Card from './components/Cards/Card';
+import Main from './components/Main/Main';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
   <>
-
+<Navbar/>
+<Main/>
+<Card/>
 
 
   </>
-    )
+)
 }
 
 export default App;
---
+++ src/components/Cards/Card.jsx
@@ -0,0 +1,12 @@
+import "./Card.css"
+
+const Card =()=>{
+    return (
+        <div className="Card">
+            <h1>Card</h1>
+        </div>
+    )
+}
+
+
+export default Card;
