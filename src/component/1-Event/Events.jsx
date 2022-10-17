db85fbb3cf2e5cf34423bc854b8327af95659a4e
Cihat <copurcihat@gmail.com>
2022-10-17 10:39:24 +0300

    ff

---
+++ src/component/1-Event/Events.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+export const Events = () => {
+  return (
+    <div className="container text-center mt-4mjjgvl">
+        <button className="btn btn-danger">Click</button>
+    </div>
+  )
+}
+
+
+export default Events;
